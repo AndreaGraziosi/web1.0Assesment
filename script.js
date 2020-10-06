fad45e97fa6330f7b254d168a337dda925c1bcbc
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-10-06 12:09:02 -0700

    added JS

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// headline element
+const display = document.querySelector("#display")
+// input field size
+const inputSize = document.querySelector("#input-size")
+const showSize = document.querySelector("#show-size")
+
+const selectFont = document.querySelector("#select-font")
+const showFont = document.querySelector("#show-font")
+
+const inputColor = document.querySelector("input-color")
+const showColor = document.querySelector("#show-color")
+
+const inputBgColor = document.querySelector("enter_bg_color")
+const showBgColor = document.querySelector("show-bg-color")
+
+const enterText = document.querySelector("#enter-text")
+
+
+
+inputSize.addEventListener("input", handleInput)
+selectFont.addEventListener("select", handleInput)
+inputColor.addEventListener("input", handleInput)
+inputBgColor.addEventListener("input", handleInput)
+
+ 
+function handleInput() {
+     const fontSize = inputSize.value
+     display.style.fontSize = fontSize
+     showSize.innerHTML = fontSize
+
+    const fontType = selectFont.value
+    display.style.fontType = fontType
+    showFont.innerHTML = fontType
+
+    const fontColor = inputColor.value
+    display.style.fontColor = fontColor
+    showColor.innerHTML = fontColor
+
+    const bgColor = inputBgColor.value
+    display.style.bgColor = bgColor
+    showBgColor.innerHTML = bgColor
+
+ }
+
